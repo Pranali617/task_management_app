47bf449a548358bcacd61c3e021457bc9237451b
Pranali Sambrekar <pranali.kangralkar@gmail.com>
2025-12-22 10:56:51 +0530

    PS: Updated the api.js

--- task-collab-frontend/src/api/api.js
+++ task-collab-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+ baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
@@ -38,7 +38,7 @@ api.interceptors.response.use(
 
         // Try to refresh the token
         const response = await axios.post(
-          `${process.env.REACT_APP_API_BASE_URL}/auth/refresh_token`,
+          `${import.meta.env.VITE_API_BASE_URL}/auth/refresh_token`,
           {},
           {
             headers: {
